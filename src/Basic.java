a5c618c350b37aa88dd40c65f0e7085422d192f7
anushkadeshpande <anushkadeshpande153@gmail.com>
2022-11-13 16:51:25 -0800

    Added basic.java

---
+++ src/Basic.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Basic
+{
+    public static void main(String [] args) throws IOException
+    {
+        System.out.println("Hello World");
+    }
+}
